67538255193d425bb52c15d8a926878333bf6733
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-15 21:25:21 +0300

    Added Basic Inheritance Codes
    Today i started to learn inheritance in Java.

---
+++ JavaDemos/Week2/inheritance/src/Customer.java
@@ -0,0 +1,9 @@
+public class Customer extends Person {
+
+    // this fields are same with Employee class
+    // int id;
+    // String firstName;
+    // String lastName;
+    // int age;
+    // String email;
+}
---
+++ JavaDemos/Week2/inheritance/src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee extends Person {
+
+    // this fields are same with Customer class
+    // int id;
+    // String firstName;
+    // String lastName;
+    // int age;
+    // String email;
+
+    double salary;
+}
