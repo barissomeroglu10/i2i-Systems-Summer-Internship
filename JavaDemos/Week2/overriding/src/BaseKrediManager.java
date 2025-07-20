9fc310a14884bdb9fec60994372924788fafe910
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-21 00:39:12 +0300

    Added "override"

---
+++ JavaDemos/Week2/overriding/src/BaseKrediManager.java
@@ -0,0 +1,7 @@
+public class BaseKrediManager {
+    // if you don't want to override for the function,
+    // you must use final keyword
+    public  double hesapla(double tutar) {
+        return tutar * 1.18;
+    }
+}
---
+++ JavaDemos/Week2/overriding/src/OgrenciKrediManager.java
@@ -0,0 +1,6 @@
+public class OgrenciKrediManager extends BaseKrediManager {
+    // overriding
+    public  double hesapla(double tutar) {
+        return tutar * 1.10;
+    }
+}
