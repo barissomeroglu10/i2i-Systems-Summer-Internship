d6b85d7961d815beb9604a007eb8ba5bb7164961
Barış Someroğlu <barissomeroglu7@gmail.com>
2025-07-22 02:56:56 +0300

    Added a Demo for Interface

---
+++ JavaDemos/Week2/interfaces/interfaceDemo/src/Worker.java
@@ -0,0 +1,18 @@
+// our workers
+public class Worker implements IWorkable, IEatable, IPayable { // multiple implementation
+
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+}
